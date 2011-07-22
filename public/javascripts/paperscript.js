d9a4574572cb2b53ea35c9478aef10fd82f35362
James Cox <th3james@gmail.com>
2011-07-22 12:03:06 +0100

    Can create points

--- public/javascripts/paperscript.js
+++ public/javascripts/paperscript.js
@@ -1,9 +1,20 @@
-var projects = [];
+//Programmes
+var programmes = [];
 
 //Create a new node
 this.createProgramme = function() {
-  var path = new Path.Circle(new Point(80, 50), 35);
-  path.fillColor = 'red';
+  var newProgramme, position, previous;
+
+  position = new Point(50, 50);
+  // If previous elements exist, move this out of the way
+  if (programmes.length > 0) {
+    previous = programmes[programmes.length - 1];
+    position = new Point(previous.position.x + 60, previous.position.y );
+  }
+  newProgramme = new Path.Circle(position, 20);
+
+  programmes.push(newProgramme);
+  newProgramme.fillColor = '#4784BC';
   view.draw();
 };
 
