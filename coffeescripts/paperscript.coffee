462038e272c4afb4dbd6bf24a6c9a01066966d0d
James Cox <th3james@gmail.com>
2011-07-22 15:34:18 +0100

    Added paperscript object container

--- coffeescripts/paperscript.coffee
+++ coffeescripts/paperscript.coffee
@@ -1,8 +1,32 @@
-class ProgrammesContainer
-  @programmes = {}
+#Generic objects container
+class Container
   constructor: (@lastPosition) ->
+    @items = {}
 
   add: (id, p) ->
     @lastPosition = p.position
-    @programmes[id] = p
+    @items[id] = p
+
+programmes = new Container(new Point(0, 50))
+
+#Create a new node
+@createProgramme = (id) ->
+  #If previous elements exist, move this out of the way
+  position = new Point(programmes.lastPosition.x + 60, programmes.lastPosition.y )
+  newProgramme = new Path.Circle(position, 20)
+
+  programmes.add(id, newProgramme)
+  newProgramme.fillColor = '#4784BC'
+  view.draw()
+
+#Create a new node
+@deleteProgramme = (id) ->
+  programme = programmes.items[id]
+  programme.remove()
+  view.draw()
+
+# Returns overlapping objects
+this.objectOverlapping = (p) -> 
+
+paper.install(window.paperscript)
 
--- public/javascripts/paperscript.js
+++ public/javascripts/paperscript.js
@@ -1,14 +1,31 @@
 (function() {
-  var ProgrammesContainer;
-  ProgrammesContainer = (function() {
-    ProgrammesContainer.programmes = {};
-    function ProgrammesContainer(lastPosition) {
+  var Container, programmes;
+  Container = (function() {
+    function Container(lastPosition) {
       this.lastPosition = lastPosition;
+      this.items = {};
     }
-    ProgrammesContainer.prototype.add = function(id, p) {
+    Container.prototype.add = function(id, p) {
       this.lastPosition = p.position;
-      return this.programmes[id] = p;
+      return this.items[id] = p;
     };
-    return ProgrammesContainer;
+    return Container;
   })();
+  programmes = new Container(new Point(0, 50));
+  this.createProgramme = function(id) {
+    var newProgramme, position;
+    position = new Point(programmes.lastPosition.x + 60, programmes.lastPosition.y);
+    newProgramme = new Path.Circle(position, 20);
+    programmes.add(id, newProgramme);
+    newProgramme.fillColor = '#4784BC';
+    return view.draw();
+  };
+  this.deleteProgramme = function(id) {
+    var programme;
+    programme = programmes.items[id];
+    programme.remove();
+    return view.draw();
+  };
+  this.objectOverlapping = function(p) {};
+  paper.install(window.paperscript);
 }).call(this);
