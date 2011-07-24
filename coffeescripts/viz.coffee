9206fbbf4c9472ceecb3cb6a1f8129a2eae60da5
James Cox <th3james@gmail.com>
2011-07-24 12:01:22 +0100

    Redraw method working, with slight namespacing hack

--- coffeescripts/viz.coffee
+++ coffeescripts/viz.coffee
@@ -1,60 +1,81 @@
 #Contains all the d3 drawing methods
 
-#Draw the graph
+root = exports ? @
+
+#Initialise the graph with some data
 @initGraph = () ->
-  w = 960
-  h = 500
-  fill = d3.scale.category20()
- 
-  vis = d3.select("#chart")
+  @graph = {}
+  @graph.w = 960
+  @graph.h = 500
+  @graph.fill = d3.scale.category20()
+
+  @vis = d3.select("#chart")
     .append("svg:svg")
-      .attr("width", w)
-      .attr("height", h)
+      .attr("width", @graph.w)
+      .attr("height", @graph.h)
   
+  #initialise the @data object with misrables.json and draw the graph
   d3.json("javascripts/miserables.json", (json) ->
-    force = d3.layout.force()
-      .charge(-120)
-      .linkDistance(30)
-      .nodes(json.nodes)
-      .links(json.links)
-      .size([w, h])
-      .start()
- 
-    link = vis.selectAll("line.link")
-      .data(json.links)
-      .enter().append("svg:line")
-      .attr("class", "link")
-      .style("stroke-width", (d) -> return Math.sqrt(d.value) )
-      .attr("x1", (d) ->  return d.source.x)
-      .attr("y1", (d) ->  return d.source.y)
-      .attr("x2", (d) ->  return d.target.x)
-      .attr("y2", (d) ->  return d.target.y)
-  
-    node = vis.selectAll("circle.node")
-      .data(json.nodes)
-      .enter().append("svg:circle")
-      .attr("class", "node")
-      .attr("cx", (d) ->  return d.x)
-      .attr("cy", (d) ->  return d.y)
-      .attr("r", 5)
-      .style("fill", (d) -> return fill(d.group))
-      .call(force.drag)
-  
-    node.append("svg:title")
-      .text((d) ->  return d.name)
-  
-    vis.style("opacity", 1e-6)
-      .transition()
-      .duration(1000)
-      .style("opacity", 1)
-  
-    force.on("tick", () ->
-      link.attr("x1", (d) ->  return d.source.x)
-      .attr("y1", (d) ->  return d.source.y)
-      .attr("x2", (d) ->  return d.target.x)
-      .attr("y2", (d) ->  return d.target.y)
- 
-      node.attr("cx", (d) ->  return d.x)
-      .attr("cy", (d) ->  return d.y)
-    )
+    @data = json
+
+    #Draw the graph with the data in place
+    redraw()
   )
+
+#Redraw the graph
+redraw = () ->
+  graph = @graph
+
+  force = d3.layout.force()
+    .charge(-120)
+    .linkDistance(30)
+    .nodes(@data.nodes)
+    .links(@data.links)
+    .size([@graph.w, @graph.h])
+    .start()
+
+  link = @vis.selectAll("line.link")
+    .data(@data.links)
+    .enter().append("svg:line")
+    .attr("class", "link")
+    .style("stroke-width", (d) -> return Math.sqrt(d.value) )
+    .attr("x1", (d) ->  return d.source.x)
+    .attr("y1", (d) ->  return d.source.y)
+    .attr("x2", (d) ->  return d.target.x)
+    .attr("y2", (d) ->  return d.target.y)
+
+  node = @vis.selectAll("circle.node")
+    .data(@data.nodes)
+    .enter().append("svg:circle")
+    .attr("class", "node")
+    .attr("cx", (d) ->  return d.x)
+    .attr("cy", (d) ->  return d.y)
+    .attr("r", 5)
+    .style("fill", (d) -> return graph.fill(d.group))
+    .call(force.drag)
+
+  node.append("svg:title")
+    .text((d) ->  return d.name)
+
+  @vis.style("opacity", 1e-6)
+    .transition()
+    .duration(1000)
+    .style("opacity", 1)
+
+  force.on("tick", () ->
+    link.attr("x1", (d) ->  return d.source.x)
+    .attr("y1", (d) ->  return d.source.y)
+    .attr("x2", (d) ->  return d.target.x)
+    .attr("y2", (d) ->  return d.target.y)
+
+    node.attr("cx", (d) ->  return d.x)
+    .attr("cy", (d) ->  return d.y)
+  )
+  
+
+@addNode = (name) ->
+  data.push(name)
+  node = @vis.selectAll("circle.node")
+    .data(json.nodes)
+    .enter().append("svg:circle")
+ 
--- public/javascripts/viz.js
+++ public/javascripts/viz.js
@@ -1,16 +1,45 @@
 (function() {
+  var redraw, root;
+  root = typeof exports !== "undefined" && exports !== null ? exports : this;
   this.initGraph = function() {
-    var fill, h, vis, w;
-    w = 960;
-    h = 500;
-    fill = d3.scale.category20();
-    vis = d3.select("#chart").append("svg:svg").attr("width", w).attr("height", h);
+    this.graph = {};
+    this.graph.w = 960;
+    this.graph.h = 500;
+    this.graph.fill = d3.scale.category20();
+    this.vis = d3.select("#chart").append("svg:svg").attr("width", this.graph.w).attr("height", this.graph.h);
     return d3.json("javascripts/miserables.json", function(json) {
-      var force, link, node;
-      force = d3.layout.force().charge(-120).linkDistance(30).nodes(json.nodes).links(json.links).size([w, h]).start();
-      link = vis.selectAll("line.link").data(json.links).enter().append("svg:line").attr("class", "link").style("stroke-width", function(d) {
-        return Math.sqrt(d.value);
-      }).attr("x1", function(d) {
+      this.data = json;
+      return redraw();
+    });
+  };
+  redraw = function() {
+    var force, graph, link, node;
+    graph = this.graph;
+    force = d3.layout.force().charge(-120).linkDistance(30).nodes(this.data.nodes).links(this.data.links).size([this.graph.w, this.graph.h]).start();
+    link = this.vis.selectAll("line.link").data(this.data.links).enter().append("svg:line").attr("class", "link").style("stroke-width", function(d) {
+      return Math.sqrt(d.value);
+    }).attr("x1", function(d) {
+      return d.source.x;
+    }).attr("y1", function(d) {
+      return d.source.y;
+    }).attr("x2", function(d) {
+      return d.target.x;
+    }).attr("y2", function(d) {
+      return d.target.y;
+    });
+    node = this.vis.selectAll("circle.node").data(this.data.nodes).enter().append("svg:circle").attr("class", "node").attr("cx", function(d) {
+      return d.x;
+    }).attr("cy", function(d) {
+      return d.y;
+    }).attr("r", 5).style("fill", function(d) {
+      return graph.fill(d.group);
+    }).call(force.drag);
+    node.append("svg:title").text(function(d) {
+      return d.name;
+    });
+    this.vis.style("opacity", 1e-6).transition().duration(1000).style("opacity", 1);
+    return force.on("tick", function() {
+      link.attr("x1", function(d) {
         return d.source.x;
       }).attr("y1", function(d) {
         return d.source.y;
@@ -19,33 +48,16 @@
       }).attr("y2", function(d) {
         return d.target.y;
       });
-      node = vis.selectAll("circle.node").data(json.nodes).enter().append("svg:circle").attr("class", "node").attr("cx", function(d) {
+      return node.attr("cx", function(d) {
         return d.x;
       }).attr("cy", function(d) {
         return d.y;
-      }).attr("r", 5).style("fill", function(d) {
-        return fill(d.group);
-      }).call(force.drag);
-      node.append("svg:title").text(function(d) {
-        return d.name;
-      });
-      vis.style("opacity", 1e-6).transition().duration(1000).style("opacity", 1);
-      return force.on("tick", function() {
-        link.attr("x1", function(d) {
-          return d.source.x;
-        }).attr("y1", function(d) {
-          return d.source.y;
-        }).attr("x2", function(d) {
-          return d.target.x;
-        }).attr("y2", function(d) {
-          return d.target.y;
-        });
-        return node.attr("cx", function(d) {
-          return d.x;
-        }).attr("cy", function(d) {
-          return d.y;
-        });
       });
     });
+  };
+  this.addNode = function(name) {
+    var node;
+    data.push(name);
+    return node = this.vis.selectAll("circle.node").data(json.nodes).enter().append("svg:circle");
   };
 }).call(this);
