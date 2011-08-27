0a14564f800cdbff238bf34c3fb2f14e0791281a
James Cox <th3james@gmail.com>
2011-08-27 18:35:26 +0100

    Moved external viz js into backbone Squashed commit of the following:
    commit ca9b94428b6bd3af77cd88e4cedf0784cc1df192
    Author: James Cox <th3james@gmail.com>
    Date:   Sat Aug 27 18:34:46 2011 +0100
    
        Removed viz files
    
    commit 7d51cb834e7eae29c9841db3e576650000f291c5
    Author: James Cox <th3james@gmail.com>
    Date:   Sat Aug 27 18:33:48 2011 +0100
    
        Moved all d3 code into backbone

--- coffeescripts/application.coffee
+++ coffeescripts/application.coffee
@@ -6,15 +6,14 @@ $(document).ready(() ->
       #Bind the remove event to the destroy and delete the html element
       @bind("remove", () ->
         $('#'+this.cid).remove()
-        window.deleteNode(this.cid)
+
         @destroy()
       )
   })
 
   #Programmes collection
   Programmes = Backbone.Collection.extend({
-    initialize: (models, options) -> 
-      this.bind("add", options.view.addProgramme)
+    initialize: (models, options) ->
   })
 
   #Application view
@@ -23,6 +22,24 @@ $(document).ready(() ->
     initialize: () ->
       #Create a programme collection when the view is initialized, with a reference to this
       @programmes = new Programmes( null, { view: this })
+
+      #Create our graph
+      @graph = {}
+      @graph.w = 960
+      @graph.h = 500
+      @graph.fill = d3.scale.category20()
+
+      @vis = d3.select("#chart")
+        .append("svg:svg")
+          .attr("width", @graph.w)
+          .attr("height", @graph.h)
+      
+      @data = {
+        nodes: [],
+        links: []
+      }
+
+      @redrawGraph()
     ,
     events: {
       "click #add_programme":  "programmeNamePrompt",
@@ -33,21 +50,89 @@ $(document).ready(() ->
       programme_model = new Programme({ name: programme_name })
       #Add a new programme model to our programmes collection
       @programmes.add( programme_model )
+      @addProgramme(programme_model)
     ,
     deleteProgramme: (e) ->
       li = $(e.currentTarget).parent('li')
       f = @programmes.getByCid(li.attr('id'))
       this.programmes.remove(f)
+      @data.nodes = @data.nodes.filter( (node) ->
+        node.id != f.cid
+      )
+      @redrawGraph()
     ,
     addProgramme: (model) ->
       #The parameter passed is a reference to the model that was added
       $("#programme_list").append("<li id='#{model.cid}'>#{model.get('name')} <a href='#' class='delete_programme'>delete</a></li>")
+
       #Add the node to the graph
-      window.addNode({name:model.get('name'), id: model.cid})
+      @data.nodes.push({name:model.get('name'), id: model.cid})
+      @redrawGraph()
+    ,
+    redrawGraph: () ->
+      graph = @graph
+
+      force = d3.layout.force()
+        .charge(-120)
+        .linkDistance(30)
+        .nodes(@data.nodes)
+        .links(@data.links)
+        .size([@graph.w, @graph.h])
+        .start()
+
+      link = @vis.selectAll("line.link")
+          .data(@data.links)
+        .enter().append("svg:line")
+          .attr("class", "link")
+          .style("stroke-width", (d) -> return Math.sqrt(d.value) )
+          .attr("x1", (d) ->  return d.source.x)
+          .attr("y1", (d) ->  return d.source.y)
+          .attr("x2", (d) ->  return d.target.x)
+          .attr("y2", (d) ->  return d.target.y)
+
+      #NODE DRAWING
+      #first we create svg:g elements as the container
+      node = @vis.selectAll("g.node")
+          .data(@data.nodes, (d) -> d.id)
+        .enter().append("svg:g")
+          .attr("class", "node")
+          .call(force.drag)
+      
+      #Then we fill the svg:g elements with:
+      #The circle
+      node.append("svg:circle")
+          .attr("r", 5)
+          .style("fill", "#234B6F")
+          .attr("class", "circle")
+          .attr("x", "-8px")
+          .attr("y", "-8px")
+          .attr("width", "16px")
+          .attr("height", "16px")
+      #The Name
+      node.append("svg:text")
+          .attr("class", "node_text")
+          .attr("dx", 12)
+          .attr("dy", ".35em")
+          .text((d) -> d.name)
+      
+      #Remove nodes
+      @vis.selectAll("g.node").data(@data.nodes, (d) -> d.id).exit().remove()
+
+      @vis.style("opacity", 1e-6)
+        .transition()
+        .duration(1000)
+        .style("opacity", 1)
+
+      force.on("tick", () ->
+        link.attr("x1", (d) ->  return d.source.x)
+        .attr("y1", (d) ->  return d.source.y)
+        .attr("x2", (d) ->  return d.target.x)
+        .attr("y2", (d) ->  return d.target.y)
+
+        node.attr("transform", (d) -> "translate(#{d.x},#{d.y})")
+      )
   })
 
-  appview = new AppView
-
-  initGraph()
+  window.appview = new AppView
 )
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,27 +1,34 @@
 (function() {
   $(document).ready(function() {
-    var AppView, Programme, Programmes, appview;
+    var AppView, Programme, Programmes;
     Programme = Backbone.Model.extend({
       name: null,
       initialize: function(options) {
         return this.bind("remove", function() {
           $('#' + this.cid).remove();
-          window.deleteNode(this.cid);
           return this.destroy();
         });
       }
     });
     Programmes = Backbone.Collection.extend({
-      initialize: function(models, options) {
-        return this.bind("add", options.view.addProgramme);
-      }
+      initialize: function(models, options) {}
     });
     AppView = Backbone.View.extend({
       el: $("body"),
       initialize: function() {
-        return this.programmes = new Programmes(null, {
+        this.programmes = new Programmes(null, {
           view: this
         });
+        this.graph = {};
+        this.graph.w = 960;
+        this.graph.h = 500;
+        this.graph.fill = d3.scale.category20();
+        this.vis = d3.select("#chart").append("svg:svg").attr("width", this.graph.w).attr("height", this.graph.h);
+        this.data = {
+          nodes: [],
+          links: []
+        };
+        return this.redrawGraph();
       },
       events: {
         "click #add_programme": "programmeNamePrompt",
@@ -33,23 +40,69 @@
         programme_model = new Programme({
           name: programme_name
         });
-        return this.programmes.add(programme_model);
+        this.programmes.add(programme_model);
+        return this.addProgramme(programme_model);
       },
       deleteProgramme: function(e) {
         var f, li;
         li = $(e.currentTarget).parent('li');
         f = this.programmes.getByCid(li.attr('id'));
-        return this.programmes.remove(f);
+        this.programmes.remove(f);
+        this.data.nodes = this.data.nodes.filter(function(node) {
+          return node.id !== f.cid;
+        });
+        return this.redrawGraph();
       },
       addProgramme: function(model) {
         $("#programme_list").append("<li id='" + model.cid + "'>" + (model.get('name')) + " <a href='#' class='delete_programme'>delete</a></li>");
-        return window.addNode({
+        this.data.nodes.push({
           name: model.get('name'),
           id: model.cid
         });
+        return this.redrawGraph();
+      },
+      redrawGraph: function() {
+        var force, graph, link, node;
+        graph = this.graph;
+        force = d3.layout.force().charge(-120).linkDistance(30).nodes(this.data.nodes).links(this.data.links).size([this.graph.w, this.graph.h]).start();
+        link = this.vis.selectAll("line.link").data(this.data.links).enter().append("svg:line").attr("class", "link").style("stroke-width", function(d) {
+          return Math.sqrt(d.value);
+        }).attr("x1", function(d) {
+          return d.source.x;
+        }).attr("y1", function(d) {
+          return d.source.y;
+        }).attr("x2", function(d) {
+          return d.target.x;
+        }).attr("y2", function(d) {
+          return d.target.y;
+        });
+        node = this.vis.selectAll("g.node").data(this.data.nodes, function(d) {
+          return d.id;
+        }).enter().append("svg:g").attr("class", "node").call(force.drag);
+        node.append("svg:circle").attr("r", 5).style("fill", "#234B6F").attr("class", "circle").attr("x", "-8px").attr("y", "-8px").attr("width", "16px").attr("height", "16px");
+        node.append("svg:text").attr("class", "node_text").attr("dx", 12).attr("dy", ".35em").text(function(d) {
+          return d.name;
+        });
+        this.vis.selectAll("g.node").data(this.data.nodes, function(d) {
+          return d.id;
+        }).exit().remove();
+        this.vis.style("opacity", 1e-6).transition().duration(1000).style("opacity", 1);
+        return force.on("tick", function() {
+          link.attr("x1", function(d) {
+            return d.source.x;
+          }).attr("y1", function(d) {
+            return d.source.y;
+          }).attr("x2", function(d) {
+            return d.target.x;
+          }).attr("y2", function(d) {
+            return d.target.y;
+          });
+          return node.attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+          });
+        });
       }
     });
-    appview = new AppView;
-    return initGraph();
+    return window.appview = new AppView;
   });
 }).call(this);
